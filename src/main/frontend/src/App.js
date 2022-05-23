de58bf76899ab05f68112bbcdba918f14c9f5adb
beyfendipenguen <beyfendipenguen@gmail.com>
2022-05-23 14:49:08 +0300

    drop file function was added for image upload

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,7 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, useCallback} from 'react';
+import {useDropzone} from 'react-dropzone'
 
 const UserProfiles = () => {
   const [userProfiles, setUserPrifles] = useState([]);
@@ -18,15 +19,42 @@ const UserProfiles = () => {
   return userProfiles.map((userProfile, index) => {
     return (
       <div key={index}>
+        {/*TODO: profile image*/}
+        <br/>
+        <br/>
         <h1>
           {userProfile.userName}
         </h1>
         <p>
           {userProfile.userProfileId}
         </p>
+        <Dropzone/>
+        <br/>
+        <br/>
+
       </div>
     )
   })
+}
+
+function Dropzone() {
+  const onDrop = useCallback(acceptedFiles => {
+    // Do something with the files
+    const file = acceptedFiles[0];
+    console.log(file);
+  }, [])
+  const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
+
+  return (
+    <div {...getRootProps()}>
+      <input {...getInputProps()} />
+      {
+        isDragActive ?
+          <p>Drop the image here ...</p> :
+          <p>Drag 'n' drop profiel image,or click to select profile image</p>
+      }
+    </div>
+  )
 }
 
 function App() {
