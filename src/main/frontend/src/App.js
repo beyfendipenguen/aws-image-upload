9e299bcd11869d6a189ee8c96ee24260ddd507ec
beyfendipenguen <beyfendipenguen@gmail.com>
2022-05-23 19:32:28 +0300

    feat: update Dropzone function for image upload

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -6,12 +6,15 @@ import {useDropzone} from 'react-dropzone'
 
 const UserProfiles = () => {
   const [userProfiles, setUserPrifles] = useState([]);
+  
+  /* updating objects and arrays in state : https://www.w3schools.com/react/react_usestate.asp */
   const fetchUserProfiles = () =>{
     axios.get("http://localhost:8080/api/v1/user-profile/").then(res => {
       console.log(res);
       setUserPrifles(res.data);
     });
   }
+  /* Runs only on the first render : https://www.w3schools.com/react/react_useeffect.asp */
   useEffect(() => {
     fetchUserProfiles();
   },[]);
@@ -28,7 +31,7 @@ const UserProfiles = () => {
         <p>
           {userProfile.userProfileId}
         </p>
-        <Dropzone/>
+        <Dropzone {...userProfile}/>
         <br/>
         <br/>
 
@@ -37,12 +40,28 @@ const UserProfiles = () => {
   })
 }
 
-function Dropzone() {
+function Dropzone({userProfileId}) {
   const onDrop = useCallback(acceptedFiles => {
-    // Do something with the files
     const file = acceptedFiles[0];
     console.log(file);
-  }, [])
+    const formData = new FormData();
+    formData.append("file",file);
+    axios
+      .post(`http://localhost:8080/api/v1/user-profile/${userProfileId}/image/upload`,
+    formData,
+    {
+      headers: {
+        "Contnet-Type": "multipart/form-data"
+      }
+    }
+    )
+    .then(res =>{
+      console.log("image upload successfully",res);
+    })
+    .catch(err=>{
+      console.log("operation failed",err);
+    });
+  },[]);
   const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
 
   return (
