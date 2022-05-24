bf49e907540a907b485099262f40cabeb297888f
beyfendipenguen <beyfendipenguen@gmail.com>
2022-05-24 12:37:21 +0300

    refactor: change user profile variables to final and delete setter methods for uuid and user name

--- src/main/java/com/beyfendipenguen/awsimageupload/profile/UserProfile.java
+++ src/main/java/com/beyfendipenguen/awsimageupload/profile/UserProfile.java
@@ -5,8 +5,8 @@ import java.util.Optional;
 import java.util.UUID;
 
 public class UserProfile {
-    private UUID userProfileId;
-    private String userName;
+    private final UUID userProfileId;
+    private final String userName;
     private String userProfileImageLink; // S3 key
 
     public UserProfile(UUID userProfileId,
@@ -21,17 +21,12 @@ public class UserProfile {
         return userProfileId;
     }
 
-    public void setUserProfileId(UUID userProfileId) {
-        this.userProfileId = userProfileId;
-    }
 
     public String getUserName() {
         return userName;
     }
 
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
+
 
     public Optional<String> getUserProfileImageLink() {
         return Optional.ofNullable(userProfileImageLink);
