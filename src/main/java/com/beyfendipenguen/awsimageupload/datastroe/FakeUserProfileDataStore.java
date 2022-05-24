e4decd55ae75f5189a895dde066a773f311f86c1
beyfendipenguen <beyfendipenguen@gmail.com>
2022-05-24 17:12:42 +0300

    chore: change fake user name

--- src/main/java/com/beyfendipenguen/awsimageupload/datastroe/FakeUserProfileDataStore.java
+++ src/main/java/com/beyfendipenguen/awsimageupload/datastroe/FakeUserProfileDataStore.java
@@ -11,8 +11,8 @@ import java.util.UUID;
 public class FakeUserProfileDataStore {
     private static final List<UserProfile> USER_PROFILES = new ArrayList<>();
     static {
-        USER_PROFILES.add(new UserProfile(UUID.fromString("145e897f-a357-49d9-8e4e-0d6e898756c8"), "MuhammedAli",null));
-        USER_PROFILES.add(new UserProfile(UUID.fromString("6e234ba9-d2d7-4b03-b7b7-f0b5765767ee"), "MikeTyson",null));
+        USER_PROFILES.add(new UserProfile(UUID.fromString("145e897f-a357-49d9-8e4e-0d6e898756c8"), "John Doe",null));
+        USER_PROFILES.add(new UserProfile(UUID.fromString("6e234ba9-d2d7-4b03-b7b7-f0b5765767ee"), "Michelle Doe",null));
     }
 
     public static List<UserProfile> getUserProfiles() {
